af39f91c9cdf02a73f4ded883e60e63a273521ab
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-09-11 23:39:26 -0300

    ah é veja só

--- src/main/java/buffer_reader/Main.java
+++ src/main/java/buffer_reader/Main.java
@@ -3,6 +3,20 @@ package buffer_reader;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+class Pessoa {
+  Double altura;
+
+  Pessoa(double altura) {
+    this.altura = altura;
+  }
+
+  @Override
+  public String toString() {
+    return this.altura.toString();
+  }
+
+}
+
 public class Main {
   public static void main(String[] args) {
     InputStreamReader input_stream_object = new InputStreamReader(System.in);
@@ -10,5 +24,9 @@ public class Main {
 
     System.out.println(input_stream_object);
     System.out.println(buffered_reader_object);
+
+    Pessoa carlos = new Pessoa(1.40);
+
+    System.out.println(carlos);
   }
 }
