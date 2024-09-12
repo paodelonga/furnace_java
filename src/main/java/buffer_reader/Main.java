644f475f67c9eba25c851d9813cbe80cd273037a
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-09-11 23:30:45 -0300

    feat: brincando com entrada de stream e leiturinhas de buffers

---
+++ src/main/java/buffer_reader/Main.java
@@ -0,0 +1,14 @@
+package buffer_reader;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) {
+    InputStreamReader input_stream_object = new InputStreamReader(System.in);
+    BufferedReader buffered_reader_object = new BufferedReader(input_stream_object);
+
+    System.out.println(input_stream_object);
+    System.out.println(buffered_reader_object);
+  }
+}
