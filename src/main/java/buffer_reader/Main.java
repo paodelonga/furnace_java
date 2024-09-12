db82bdbceeed58c615f4eb17c9099a5aee071041
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-09-11 23:42:13 -0300

    texto entra texto sai a vida é assim (talvez não seja sobre textos)

--- src/main/java/buffer_reader/Main.java
+++ src/main/java/buffer_reader/Main.java
@@ -1,32 +1,15 @@
 package buffer_reader;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Pessoa {
-  Double altura;
-
-  Pessoa(double altura) {
-    this.altura = altura;
-  }
-
-  @Override
-  public String toString() {
-    return this.altura.toString();
-  }
-
-}
-
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     InputStreamReader input_stream_object = new InputStreamReader(System.in);
     BufferedReader buffered_reader_object = new BufferedReader(input_stream_object);
 
     System.out.println(input_stream_object);
-    System.out.println(buffered_reader_object);
-
-    Pessoa carlos = new Pessoa(1.40);
-
-    System.out.println(carlos);
+    System.out.println(buffered_reader_object.readLine());
   }
 }
