95b8d3716b4abb3c4ab006d09b82d134a2198174
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-10-02 09:52:17 -0300

    remove coisitas nao usadas

--- src/main/java/poor/Main.java
+++
@@ -1,6 +0,0 @@
-package poor;
-
-public class Main {
-  public static void main(String[] args) {
-  }
-}
--- src/main/java/ticket/Main.java
+++
@@ -1,7 +0,0 @@
-package ticket;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
