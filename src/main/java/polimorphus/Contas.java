79b8df0fdb9a5384adf2b4e171dc4c9c97ce4a23
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-10-02 11:24:43 -0300

    feat mostrarCalculo agora aceita tipo de ref OperacaoMatematica aeeee

--- src/main/java/polimorphus/Contas.java
+++ src/main/java/polimorphus/Contas.java
@@ -1,13 +1,14 @@
 package polimorphus;
 
 public class Contas {
-//    public static void mostrarCalculo(OperacaoMatematica op, double x, double y) {
-//        System.out.println("O resultado da operacao é: " + op.calcular(x, y));
-//    }
+    public static void mostrarCalculo(OperacaoMatematica op, double x, double y) {
+        System.out.println("O resultado da operacao é: " + op.calcular(x, y));
+    }
 
     public static void mostrarCalculo(Soma soma, double x, double y) {
         System.out.println("O Resultado é: " + soma.calcular(x, y));
     }
+
     public static void mostrarCalculo(Subtracao subtracao, double x, double y) {
         System.out.println("O Resultado é: " + subtracao.calcular(x, y));
     }
