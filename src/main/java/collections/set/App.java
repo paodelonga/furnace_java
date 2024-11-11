6ba0f07c745327d79f214b63eecd1174a36de26b
paodelonga <72471292+paodelonga@users.noreply.github.com>
2024-11-10 23:33:05 -0300

    me deixe testar uma coisia
    Signed-off-by: paodelonga <72471292+paodelonga@users.noreply.github.com>

--- src/main/java/collections/set/App.java
+++ src/main/java/collections/set/App.java
@@ -3,6 +3,7 @@ package collections.set;
 import collections.Pessoa;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class App {
     private static HashSet<Pessoa> pessoas;
@@ -23,6 +24,7 @@ public class App {
         pessoas = new HashSet<Pessoa>();
 
         Pessoa p1 = new Pessoa("Gabriel", 12);
+
         adicionarPessoa(p1);
         adicionarPessoa(p1);
 
@@ -30,5 +32,12 @@ public class App {
         adicionarPessoa(new Pessoa("Gabriel", 13));
 
         adicionarPessoa(new Pessoa("Gabriel", null));
+
+        Pessoa p2 = new Pessoa("Gabriel", 12);
+
+        System.out.println(p1.equals(p2));
+        System.out.println(p2.equals(p1));
+        System.out.println(p1.equals(null));
+
     }
 }
